273199a63f443277830600e2c0e4c050bae0c45b
Niklas Amann <niklas.amann95@gmail.com>
2018-06-25 23:02:56 +0200

    Entzippen der doxc in PHP StatChecker2.php

--- StatChecker.php
+++ StatChecker.php
@@ -1,34 +1,42 @@
 <html>
- <head>
-  <title>Statchecker</title>
-  <script type="text/javascript" language="javascript" src="../Saxonce/Saxonce.nocache.js"></script>
-  <!--<script type="application/xslt+xml" language="xslt2.0" src="StatChecker.xsl" data-source="document.xml"></script>-->
- </head>
- <body>
-	<h2>Statchecker</h2>
-	<form action="StatChecker.php" method="post">
-		Spieler: <input type="text" name="spieler">
-		<input type="submit" value="Submit">
-	</form> 
+	<head>
+		<title>Statchecker</title>
+		<script type="text/javascript" language="javascript" src="../Saxonce/Saxonce.nocache.js"></script>
+	</head>
+	<body>
+		<h2>Statchecker</h2>
+		<form action="StatChecker.php" method="post">
+			Spieler: 
+			<input type="text" name="spieler">
+			<input type="submit" value="Submit">
+		</form> 
 	
-	<?php
-	$spieler = $_POST['spieler'];
-	?>
-	
-	<script>
-	var t = '<?php echo $spieler;?>';
-	</script>
-	
-	<script> 
-	var onSaxonLoad = function() { 
-		Saxon.run( { 
-			stylesheet: "StatChecker.xsl", 
-			source: "document.xml",
-			parameters: {spieler: t}
-		}); 
-	}
-	</script>
-	
-	
- </body>
+		<?php
+			$spieler = $_POST['spieler'];
+			
+			$file = 'Basketball_Tabellen.docx';
+			$newfile = 'Basketball_Tabellen.zip';
+			copy($file, $newfile);
+			
+			$zip = new ZipArchive;
+			if($zip->open('Basketball_Tabellen.zip') === TRUE) {
+				$zip->extractTo('Basketball_Tabellen/');
+				$zip->close();
+			}
+		?>
+		
+		<script>
+			var t = '<?php echo $spieler;?>';
+		</script>
+		
+		<script> 
+			var onSaxonLoad = function() { 
+				Saxon.run( { 
+					stylesheet: "StatChecker.xsl", 
+					source: "Basketball_Tabellen/word/document.xml",
+					parameters: {spieler: t}
+				}); 
+			}
+		</script>	
+	</body>
 </html>
